cd117c44d2f70bf434c7545b2fa2288315d23d1c
tawakal <fairtawakal@gmail.com>
2021-02-11 21:49:44 +0700

    spring boot

---
+++ src/test/java/com/mit/jwt/BelajarJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mit.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
