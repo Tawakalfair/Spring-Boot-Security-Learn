9325ae3067b850e4fe35245c1520f5094fdeaef2
tawakal <fairtawakal@gmail.com>
2021-02-12 21:16:05 +0700

    preauthorize

--- src/main/java/com/mit/jwt/controller/StudentManagementController.java
+++ src/main/java/com/mit/jwt/controller/StudentManagementController.java
@@ -1,6 +1,7 @@
 package com.mit.jwt.controller;
 
 import com.mit.jwt.model.Student;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -17,21 +18,25 @@ public class StudentManagementController {
     );
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('ROLe_ADMIN','ROLE_ADMINTRAINEE')")
     public List<Student> getStudents(){
         return STUDENTS;
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('student:write')")
     public void registerNewStudent(@RequestBody Student student){
         System.out.println(student);
     }
 
     @PutMapping(path="{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void updateStudent(@PathVariable("studentId") Integer studentId, @RequestBody Student student){
         System.out.printf("%s %s",studentId,student);
     }
 
     @DeleteMapping(path="{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void deleteStudent(@PathVariable Integer studentId){
         System.out.println(studentId);
     }
